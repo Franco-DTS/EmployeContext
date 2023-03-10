a48b1d4dcccefa2734b7eaee26558754e761f714
Franco-DTS <jafh79258@gmail.com>
2023-03-10 14:41:24 -0600

    push 1
    tarjeta finalizada

---
+++ employecard/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import EmployeeCard from './components/EmployeCard.tsx';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <EmployeeCard />
+    </div>
+  );
+}
+
+export default App;
